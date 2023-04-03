5d7f3031a09866e5ee3f66d0eaa5314832797d53
FloThomas80 <70484942+FloThomas80@users.noreply.github.com>
2023-04-03 16:41:03 +0200

    update on test to get results from url

--- Assets/Scripts/GetJSON.cs
+++ Assets/Scripts/GetJSON.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 using UnityEngine.Networking;
 using SimpleJSON;
 using System.Threading.Tasks;
@@ -11,41 +12,50 @@ public class GetJSON : MonoBehaviour
     [ContextMenu("Test Get")]
     void Start()
     {
-        //StartCoroutine(GetRequest("https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current_weather=true&hourly=temperature_2m,relativehumidity_2m,windspeed_10m"));
+        //StartCoroutine(GetRequest("https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current_weather=true&hourly=temperature_2m,relativehumidity_2m,windspeed_10m")); // methode 1
+
+        StartCoroutine("SendRequest");
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+//    IEnumerator GetRequest (string url) // methode 1
+//    {
+//        using (UnityWebRequest webRequest = UnityWebRequest.Get(url))
+//        {
+//            yield return webRequest.SendWebRequest();
 
-    public async void TestGet()
+//            Debug.Log(webRequest.downloadHandler.text);
+//            var N = JSON.Parse(webRequest.downloadHandler.text);
+//            string weather  = N["current_weather"][0]["temperature"].Value;
+//            print(weather);
+//        }
+//    }
+//}
+
+    IEnumerator SendRequest ()
     {
         string url1 = "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current_weather=true&hourly=temperature_2m,relativehumidity_2m,windspeed_10m";
+        //Debug.Log("etape 1 ok");
         using var www = UnityWebRequest.Get(url1);
 
-        www.SetRequestHeader("content-type","application/JSON");
+        www.SetRequestHeader("content-type", "application/JSON");
         var operation = www.SendWebRequest();
-
+     
         while (!operation.isDone)
-            await Task.Yield();
+            yield return Task.Yield();
+    
+
 
         if (www.result == UnityWebRequest.Result.Success)
             Debug.Log("Succes : " + www.downloadHandler.text);
         else
             Debug.Log("Failure");
     }
-    IEnumerator GetRequest (string url)
-    {
-        using (UnityWebRequest webRequest = UnityWebRequest.Get(url))
-        {
-            yield return webRequest.SendWebRequest();
 
-            Debug.Log(webRequest.downloadHandler.text);
-            var N = JSON.Parse(webRequest.downloadHandler.text);
-            string weather  = N["current_weather"][0]["temperature"].Value;
-            print(weather);
-        }
-    }
+//public class Button : Editor
+//{
+//    public override void OnInspectorGUI()
+//    {
+//        EditorGUILayout.LabelField("Send request");
+//    }
+
 }
